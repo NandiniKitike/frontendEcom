f5554e4d8e89d6b48777ce27be5807d8cf8f62eb
NandiniKitike2001 <Nandini@taxiondc.com>
2025-06-30 22:58:17 +0530

    url

--- constant.js
+++ constant.js
@@ -1,3 +1,3 @@
-export const BASE_URL = "http://localhost:5000";
+export const BASE_URL = "https://e-commerce-mbth.onrender.com";
 // https://e-commerce-mbth.onrender.com
 // "http://localhost:5000";
